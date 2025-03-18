0d66a3a5fd394e6b71726cf93b985e9ba9f19d4d
HE161655-BuiTuanSon <sonbthe161655@fpt.edu.vn>
2025-03-18 22:27:11 +0700

    categoryAdapter + layout

--- app/src/main/java/com/example/onlineclothesorder/DAO/AppDatabase.java
+++ app/src/main/java/com/example/onlineclothesorder/DAO/AppDatabase.java
@@ -1,13 +1,27 @@
 package com.example.onlineclothesorder.DAO;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.onlineclothesorder.Entity.Cart;
+import com.example.onlineclothesorder.Entity.Category;
 import com.example.onlineclothesorder.Entity.Order;
 import com.example.onlineclothesorder.Entity.Product;
 
-@Database(entities = {Product.class, Cart.class, Order.class}, version = 1)
+@Database(entities = {Product.class, Cart.class, Order.class, Category.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase instance;
     public abstract AppDAO appDAO();
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "shopping_db")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return instance;
+    }
 }
